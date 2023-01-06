1f6aa80e3af5b4ab44068e383512caca1df7827c
Jim Garner <jim@amberengine.com>
2023-01-06 16:23:08 -0500

    initial commit

--- udemy_cdk/udemy_cdk_stack.py
+++ udemy_cdk/udemy_cdk_stack.py
@@ -1,7 +1,6 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
 )
 from constructs import Construct
 
@@ -10,10 +9,10 @@ class UdemyCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "UdemyCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        s3.Bucket(
+            self,
+            "JimFirstBucket",
+            bucket_name="jim-first-bucket-101010",
+            versioned=True,
+            encryption=s3.BucketEncryption.KMS_MANAGED
+        )
